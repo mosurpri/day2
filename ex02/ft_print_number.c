7dc0929fd032e181503d5b39cb0ec5ddb0feb213
Mogau surprise <mosurpri@c4r25s1.wethinkcode.co.za>
2020-07-10 16:06:03 +0200

    day2 work

---
+++ ex02/ft_print_number.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void ft_putchar(char ch)
+{
+	write(1,&ch, 1)
+}
+void ft_print_number(void)
+{
+	char number ='0';
+	while (number < '9')
+	{
+		ft_putchar(number);
+		number++;
+	}
+}
+int main(void)
+{
+	ft_print_alphabet();
+	return 0;
+}
